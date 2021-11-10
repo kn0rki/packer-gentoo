4b66651e4fac595e5da8afd0c622cf2abd04acdb
Sascha Hoppe <kn0rki@users.noreply.github.com>
2021-11-10 14:58:44 +0100

    test

--- scripts/provision_gentoo.sh
+++ scripts/provision_gentoo.sh
@@ -52,7 +52,8 @@ mount /dev/sda3 /mnt/gentoo
 mkdir -p /mnt/gentoo/boot
 mount /dev/sda1 /mnt/gentoo/boot
 
-  curl -SsLl "https://mirror.netcologne.de/gentoo/releases/amd64/autobuilds/current-stage3-amd64-systemd/$(curl -SsLl https://mirror.netcologne.de/gentoo/releases/amd64/autobuilds/current-stage3-amd64-systemd/ | grep 'href="stage3-amd64-systemd' | head -n 1 | cut -d '"' -f 2)" | tar xpJ -C /mnt/gentoo --xattrs --numeric-owner && break
+FOO="$(curl -SsLl https://mirror.netcologne.de/gentoo/releases/amd64/autobuilds/current-stage3-amd64-systemd/ | grep 'href="stage3-amd64-systemd' | head -n 1 | cut -d '"' -f 2)"
+curl -SsLl "https://mirror.netcologne.de/gentoo/releases/amd64/autobuilds/current-stage3-amd64-systemd/`echo $FOO`" | tar xpJ -C /mnt/gentoo --xattrs --numeric-owner && break
 
 
 # modify the chroot with some custom settings
